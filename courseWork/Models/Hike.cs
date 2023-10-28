8c414073ce79981db9d14aecf100380b4c5eb1cf
tambintv123 <tamnguyen.10092000tv@gmail.com>
2023-10-28 10:44:26 +0700

    done

---
+++ courseWork/Models/Hike.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System;
+namespace courseWork.Models
+{
+    public class Hike
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DateStart { get; set; }
+        public bool IsPacking { get; set; }
+        public double LengthOfHike { get; set; }
+        public string LevelOfDifficulty { get; set; }
+        public string Description { get; set; }
+
+        public Hike Clone() => MemberwiseClone() as Hike;
+
+        public (bool IsValid, string? ErrorMessage) Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return (false, $"{nameof(Name)} is required.");
+            }
+            else if (string.IsNullOrWhiteSpace(Location))
+            {
+                return (false, $"{nameof(Location)} is required.");
+            }
+            else if (LengthOfHike <= 0)
+            {
+                return (false, $"{nameof(LengthOfHike)} should be greater than 0.");
+            }
+            else if (DateStart == DateTime.MinValue)
+            {
+                return (false, $"{nameof(DateStart)} is required.");
+            }
+            else if (string.IsNullOrWhiteSpace(LevelOfDifficulty))
+            {
+                return (false, $"{nameof(LevelOfDifficulty)} is required.");
+            }
+         
+            return (true, null);
+
+        }
+    }
+}
