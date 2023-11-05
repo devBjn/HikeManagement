dfdb3d4e4e02bb4c0c928119d4540926d910354c
tambintv123 <tamnguyen.10092000tv@gmail.com>
2023-11-05 14:22:36 +0700

    update datepicker

--- courseWork/Models/Hike.cs
+++ courseWork/Models/Hike.cs
@@ -4,6 +4,11 @@ namespace courseWork.Models
 {
     public class Hike
     {
+        public Hike ()
+        {
+            DateStart = DateTime.Now;
+        }
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
